74a3b456b05a24cb184b0fe38b969fe4e8bb3430
AnalogMemory <alex@analogmemory.xyz>
2018-02-11 22:40:16 -0800

    Header styles updated to reflect design

--- templates/header.php
+++ templates/header.php
@@ -11,25 +11,29 @@ global $images;
 <header id="masthead" class="header" role="banner">
   <div class="header-inner">
     <figure class="logo">
-      <a href="<?php echo esc_url( home_url('/') ); ?>" rel="home" title="<?php bloginfo('name'); ?>">
-        <?php echo $images->inlineSvg('logo.svg'); ?>
-      </a>
+      <?php
+        $logo = $images->imagePath('logo-nst.png');
+        if ($logo) :
+          printf(__('<a href="%s" rel="home" title="%s"><img src="%s" alt="" /></a>', 'ntsp'),
+            esc_url(get_home_url('/')),
+            esc_attr(get_bloginfo('name')),
+            esc_url($logo));
+        endif;
+      ?>
     </figure>
 
     <nav class="main-navigation" role="navigation">
       <button data-menu="primary-menu" class="ani-menu-icon ani-menu-icon--x">
         <span></span>
       </button>
-      <div class="menu-container">
-        <?php
-          wp_nav_menu(array(
-            'theme_location' => 'primary',
-            'menu_id' => 'menu-primary',
-            'menu_class' => 'menu menu-primary',
-            'container' => '',
-          ));
-        ?>
-      </div>
+      <?php
+        wp_nav_menu(array(
+          'theme_location' => 'primary',
+          'menu_id' => 'menu-primary',
+          'menu_class' => 'menu menu-primary',
+          'container' => NULL,
+        ));
+      ?>
     </nav>
   </div>
 </header>
