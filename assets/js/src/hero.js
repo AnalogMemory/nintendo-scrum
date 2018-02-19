ccea748ab259583cb656e6049ce3d951fd2804e3
AnalogMemory <alex@analogmemory.xyz>
2018-02-19 12:32:20 -0800

    Change speed of hero parallax images

--- assets/js/src/hero.js
+++ assets/js/src/hero.js
@@ -1,5 +1,7 @@
 import Rellax from 'rellax'
 
 (function () {
-  const rellax = new Rellax('.rellax') // eslint-disable-line no-unused-vars
+  const rellax = new Rellax('.rellax', { // eslint-disable-line no-unused-vars
+    speed: -8
+  })
 })()
