215d072415ca5c3b77721c15c1e75a4aee7ffbf1
AnalogMemory <alex@analogmemory.xyz>
2018-02-20 09:13:56 -0800

    Adds enquire.js to create a responisve layout for parallax images. For screens less than 640px it's turned off

--- assets/js/src/hero.js
+++ assets/js/src/hero.js
@@ -1,7 +1,12 @@
 import Rellax from 'rellax'
+import Enquire from 'enquire.js'
 
 (function () {
-  const rellax = new Rellax('.rellax', { // eslint-disable-line no-unused-vars
-    speed: -8
+  Enquire.register('screen and (min-width:640px)', {
+    match: function () {
+      const rellax = new Rellax('.rellax', { // eslint-disable-line no-unused-vars
+        speed: -8
+      })
+    }
   })
 })()
