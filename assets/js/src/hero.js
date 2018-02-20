c14d1dfec383c71099b9ecaad36383988ff183a7
AnalogMemory <alex@analogmemory.xyz>
2018-02-20 10:05:00 -0800

    Add conditional for pages without parallax

--- assets/js/src/hero.js
+++ assets/js/src/hero.js
@@ -2,11 +2,15 @@ import Rellax from 'rellax'
 import Enquire from 'enquire.js'
 
 (function () {
-  Enquire.register('screen and (min-width:640px)', {
-    match: function () {
-      const rellax = new Rellax('.rellax', { // eslint-disable-line no-unused-vars
-        speed: -8
-      })
-    }
-  })
+  const parallaxImage = document.querySelector('.rellax')
+
+  if (parallaxImage) {
+    Enquire.register('screen and (min-width:640px)', {
+      match: function () {
+        const rellax = new Rellax('.rellax', { // eslint-disable-line no-unused-vars
+          speed: -8
+        })
+      }
+    })
+  }
 })()
